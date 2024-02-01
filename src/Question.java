1cfc1d1fd0157fbd24dbf1106cc4a5af42aac134
Nimet Seyrek <105562039+nimetts@users.noreply.github.com>
2024-02-01 14:23:36 +0300

    Commit 1

---
+++ src/Question.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Question {
+    public static void main(String [] args){
+   Scanner input = new Scanner(System.in);
+   int number1,number2;
+   int sum=0;
+   System.out.println("Enter first number ");
+   number1= input.nextInt();
+
+    }
+}
